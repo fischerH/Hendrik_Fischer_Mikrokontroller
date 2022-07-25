000c975d506f2b1886b9dddd436aee9914f2feaf
Hendrik Fischer <hendrik.fischer@ksat-stuttgart.de>
2022-07-25 15:23:50 +0200

    deleted Magnetometer files

--- Debug/Core/Src/subdir.mk
+++ Debug/Core/Src/subdir.mk
@@ -7,7 +7,6 @@
 C_SRCS += \
 ../Core/Src/FXOS8700CQ.c \
 ../Core/Src/Gyro.c \
-../Core/Src/Magnetometer.c \
 ../Core/Src/main.c \
 ../Core/Src/stm32f0xx_hal_msp.c \
 ../Core/Src/stm32f0xx_it.c \
@@ -18,7 +17,6 @@ C_SRCS += \
 OBJS += \
 ./Core/Src/FXOS8700CQ.o \
 ./Core/Src/Gyro.o \
-./Core/Src/Magnetometer.o \
 ./Core/Src/main.o \
 ./Core/Src/stm32f0xx_hal_msp.o \
 ./Core/Src/stm32f0xx_it.o \
@@ -29,7 +27,6 @@ OBJS += \
 C_DEPS += \
 ./Core/Src/FXOS8700CQ.d \
 ./Core/Src/Gyro.d \
-./Core/Src/Magnetometer.d \
 ./Core/Src/main.d \
 ./Core/Src/stm32f0xx_hal_msp.d \
 ./Core/Src/stm32f0xx_it.d \
@@ -45,7 +42,7 @@ Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/FXOS8700CQ.d ./Core/Src/FXOS8700CQ.o ./Core/Src/FXOS8700CQ.su ./Core/Src/Gyro.d ./Core/Src/Gyro.o ./Core/Src/Gyro.su ./Core/Src/Magnetometer.d ./Core/Src/Magnetometer.o ./Core/Src/Magnetometer.su ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/stm32f0xx_hal_msp.d ./Core/Src/stm32f0xx_hal_msp.o ./Core/Src/stm32f0xx_hal_msp.su ./Core/Src/stm32f0xx_it.d ./Core/Src/stm32f0xx_it.o ./Core/Src/stm32f0xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f0xx.d ./Core/Src/system_stm32f0xx.o ./Core/Src/system_stm32f0xx.su
+	-$(RM) ./Core/Src/FXOS8700CQ.d ./Core/Src/FXOS8700CQ.o ./Core/Src/FXOS8700CQ.su ./Core/Src/Gyro.d ./Core/Src/Gyro.o ./Core/Src/Gyro.su ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/stm32f0xx_hal_msp.d ./Core/Src/stm32f0xx_hal_msp.o ./Core/Src/stm32f0xx_hal_msp.su ./Core/Src/stm32f0xx_it.d ./Core/Src/stm32f0xx_it.o ./Core/Src/stm32f0xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f0xx.d ./Core/Src/system_stm32f0xx.o ./Core/Src/system_stm32f0xx.su
 
 .PHONY: clean-Core-2f-Src
 
